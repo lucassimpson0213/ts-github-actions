c0c042a3013d2fb39b3750ba18fd02699efae21d
Lucas Simpson <113938275+lucassimpson0213@users.noreply.github.com>
2026-02-19 22:55:50 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules']
+    ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules', '**/_disabled']
   },
   ...compat.extends(
     'eslint:recommended',
